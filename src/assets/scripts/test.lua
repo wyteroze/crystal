8e47f5d475c23ffab86ff8315826e44cc9dac922
wyteroze <me@wyteroze.net>
2026-07-15 15:53:50 -0400

    Delete test.lua

--- src/assets/scripts/test.lua
+++
@@ -1,9 +0,0 @@
---[[
-Copyright 2026 wyteroze. Licensed under the Apache License, Version 2.0.
---]]
-
-print(Test.coolNum)
-print(Test:doSomething(5, 10))
-print(Test.coolNum)
-
-print(Assets.loadMesh("hello"))
