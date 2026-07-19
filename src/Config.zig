92e7fc5a905132e09036db0bd7298ac2ba1a7fa7
wyteroze <me@wyteroze.net>
2026-07-19 15:21:18 -0400

    Remove resolution and scale from config. Also version is officially v0.0.1

--- src/Config.zig
+++ src/Config.zig
@@ -7,9 +7,6 @@ const log = @import("log.zig").config;
 
 pub const Config = struct {
     fps: u32,
-    width: u16,
-    height: u16,
-    scale: f32,
 
     pub fn load(allocator: std.mem.Allocator, io: anytype, path: []const u8) !Config {
         var toml = TomlData.loadFromFile(allocator, io, path) catch |e| {
@@ -23,21 +20,8 @@ pub const Config = struct {
             break :blk TomlValue{ .integer = 60 };
         };
 
-        const resolution = (toml.get("window.resolution") orelse blk: {
-            log.warn("window.resolution missing, defaulting to [ 540, 360 ]", .{});
-            break :blk TomlValue{ .array = @constCast(&[_]TomlValue{ .{ .integer = 540 }, .{ .integer = 360 } }) };
-        }).array;
-
-        const scale = (toml.get("window.scale") orelse blk: {
-            log.warn("window.scale missing, defaulting to 1.0", .{});
-            break :blk TomlValue{ .float = 1.0 };
-        }).float;
-
         return .{
-            .fps = @intCast(fps.integer),
-            .width = @intCast(resolution[0].integer),
-            .height = @intCast(resolution[1].integer),
-            .scale = @floatCast(scale),
+            .fps = @intCast(fps.integer)
         };
     }
 };
